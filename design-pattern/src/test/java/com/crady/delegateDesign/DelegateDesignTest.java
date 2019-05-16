efafffc84614401ee4dc5e1ded9fc41f897b3ddb
zhouming <Crady35200@163.com>
2019-05-16 11:49:58 +0800

    add delegate design pattern

---
+++ design-pattern/src/test/java/com/crady/delegateDesign/DelegateDesignTest.java
@@ -0,0 +1,21 @@
+package com.crady.delegateDesign;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author :Crady
+ * date :2019/5/16 11:30
+ * desc :主要角色有三种: 抽象任务角色, 委派者角色, 具体任务角色.
+ * 优点: 对内隐藏实现, 易于扩展; 简化调用;
+ **/
+public class DelegateDesignTest {
+
+    @Test
+    public void delegateDesignTest(){
+        Manager manager = new Manager();
+        manager.execute("order");
+        manager.execute("pay");
+    }
+}
