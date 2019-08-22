9bad92c31acbe36265f07be2ff5248b14e3c4280
zhouming <Crady35200@163.com>
2019-08-22 10:25:53 +0800

    模拟死锁

---
+++ thread-concurrent/src/main/java/com/crady/deadlock/DeadLock1.java
@@ -0,0 +1,40 @@
+package com.crady.deadlock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author :Crady
+ * date :2019/8/22 10:15
+ * desc : 使用Synchronized模拟死锁
+ **/
+public class DeadLock1 {
+
+    private static Object a = new Object();
+    private static Object b = new Object();
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            synchronized (a){
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (b){
+                    System.out.println("1");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (b){
+                synchronized (a){
+                    System.out.println("2");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+}
