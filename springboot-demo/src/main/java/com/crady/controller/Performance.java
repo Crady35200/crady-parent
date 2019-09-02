6d0eb56ffa608bbbe0001470297d96e7f904e640
zhouming <Crady35200@163.com>
2019-09-02 18:14:14 +0800

    集成Swagger

--- springboot-demo/src/main/java/com/crady/controller/Performance.java
+++ springboot-demo/src/main/java/com/crady/controller/Performance.java
@@ -1,4 +1,4 @@
-package com.crady.performance;
+package com.crady.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
