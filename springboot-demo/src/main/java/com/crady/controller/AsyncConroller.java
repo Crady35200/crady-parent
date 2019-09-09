b589226a8d7d1eac10818c58435760ed96774b8b
zhouming <Crady35200@163.com>
2019-09-09 10:05:38 +0800

    异步调用优化

--- springboot-demo/src/main/java/com/crady/controller/AsyncConroller.java
+++ springboot-demo/src/main/java/com/crady/controller/AsyncConroller.java
@@ -1,6 +1,7 @@
 package com.crady.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,11 @@ import java.util.concurrent.TimeUnit;
 public class AsyncConroller {
 
     @GetMapping("demo")
+    @Async
     public String asyncDemo(){
         String msg = "异步调用...";
         try {
-            TimeUnit.SECONDS.sleep(5000);
+            TimeUnit.SECONDS.sleep(5);
             log.info(msg);
         } catch (InterruptedException e) {
             e.printStackTrace();
