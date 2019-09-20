6b6c8905256c9d156116f249863b790a27f7b7bf
zhouming <Crady35200@163.com>
2019-09-20 14:03:19 +0800

    添加springbean生命周期注释和活动图

--- spring-demo/src/main/java/com/crady/annotation/beanCycle/BeanCycleMainConfig.java
+++ spring-demo/src/main/java/com/crady/annotation/beanCycle/BeanCycleMainConfig.java
@@ -10,7 +10,19 @@ import org.springframework.context.annotation.Lazy;
 /**
  * author:Crady
  * date:2019/08/01 23:20
- * desc:
+ * desc: 执行顺序：
+ *   bean construct>
+ *   BeanNameAware-setBeanName>
+ *   BeanFactoryAware-setBeanFactory>
+ *   ApplicationContextAware-setApplicationContext>
+ *   BeanPostProcessor-postProcessBeforeInitialization(对所有bean生效)>
+ *   JSR-250-postConstruct>
+ *   InitializingBean-afterPropertiesSet>
+ *   init method>
+ *   BeanPostProcessor-postProcessAfterInitialization(对所有bean生效)>
+ *   JSR-250-PreDestroy>
+ *   DisposableBean-destroy>
+ *   destroy method
  **/
 @Configuration
 @ComponentScan("com.crady.annotation.beanCycle")
