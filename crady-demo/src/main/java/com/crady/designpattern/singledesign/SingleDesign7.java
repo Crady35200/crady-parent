26c01ba7268e5957cc572e4b06da2d79739513e3
zhouming <Crady35200@163.com>
2019-10-18 09:24:28 +0800

    单例模式-内部类实现优缺点

--- crady-demo/src/main/java/com/crady/designpattern/singledesign/SingleDesign7.java
+++ crady-demo/src/main/java/com/crady/designpattern/singledesign/SingleDesign7.java
@@ -9,7 +9,9 @@ package com.crady.designpattern.singledesign;
  * 而静态内部类方式在Singleton类被装载时并不会立即实例化，而是在需要实例化时，调用getInstance方法，
  * 才会装载SingletonInstance类，从而完成Singleton的实例化。
  * 优点：在避免了线程不安全，延迟加载，效率高
- * 缺点：
+ * 由于在调用 SingletonHolder.instance 的时候，才会对单例进行初始化，而且通过反射，
+ * 是不能从外部类获取内部类的属性的。 所以这种形式，很好的避免了反射入侵。
+ * 缺点：需要两个类去做到这一点，虽然不会创建静态内部类的对象，但是其 Class 对象还是会被创建，而且是属于永久代的对象。
  **/
 public class SingleDesign7 {
 
