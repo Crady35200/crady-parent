4b3eb1c4703a1ec3e3cc001c22f3721e7304e1a2
crady <Crady35200@163.com>
2022-11-29 11:34:31 +0800

    算法：寻找最大无重复子串优化

--- crady-demo/src/main/java/com/crady/algorithm/example/NoRepeatMaxLengthSubString.java
+++ crady-demo/src/main/java/com/crady/algorithm/example/NoRepeatMaxLengthSubString.java
@@ -9,17 +9,26 @@ public class NoRepeatMaxLengthSubString {
 
     public static void main(String[] args) {
 
-        getMaxSubStringLength("pwwkew");
-        getMaxSubStringLength("");
-        getMaxSubStringLength("eeee");
+        System.out.println("*********getMaxSubStringLength**********");
+        System.out.println(getMaxSubStringLength("abcabcbb"));
+        System.out.println(getMaxSubStringLength("pwwkew"));
+        System.out.println(getMaxSubStringLength(""));
+        System.out.println(getMaxSubStringLength("eeee"));
+
+        System.out.println("*********maxSubString**********");
+        System.out.println(maxSubString("abcabcbb"));
+        System.out.println(maxSubString("pwwkew"));
+        System.out.println(maxSubString(""));
+        System.out.println(maxSubString("eeee"));
 
     }
 
     public static int getMaxSubStringLength(String s) {
-        int max = 0;
-        if (s == null){
-            max = 0;
+
+        if (s == null || s.length() < 1){
+            return 0;
         }
+        int max = 1;
         for (int i = 0; i < s.length(); i++) {
             int n = 1;
             for (int j = i + 1; j < s.length(); j++) {
@@ -31,7 +40,29 @@ public class NoRepeatMaxLengthSubString {
             }
             max = max < n ? n : max;
         }
-        System.out.println(max);
+        return max;
+    }
+
+    public static int maxSubString(String s) {
+        if(s == null || s.length() < 1) {
+            return 0;
+        }
+        int len = s.length();
+        int max = 1;
+        String str = "";
+        int i=0,j=0;
+        while(i<len && j<len) {
+            String tmp = "" + s.charAt(j);
+            if (str.contains(tmp)) {
+                str = "";
+                i++;
+                j=i;
+            }else{
+                max = Math.max(max,j-i + 1);
+                str = str + tmp;
+                j++;
+            }
+        }
         return max;
     }
 
